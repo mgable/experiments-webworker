798eedf0d86573c6c8f5f4bf95b11b0a3f1d2d70
Mark Gable <mgable43@gmail.com>
2016-11-04 05:50:54 -0700

    get it to write to fs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require('express'),
+	fs = require("fs");
+
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/tasks', function(req, res, next) {
+	res.render('index', { title: 'tasks engine' });
+});
+
+router.post('/tasks', function(req, res, next) {
+	try{
+	res.contentType("application/json; charset=UTF-8");
+	fs.writeFileSync(__dirname + "/../data/test.json", JSON.stringify(req.body) ,'utf8')
+	res.send({"hello": "world"});
+}catch(e){console.error(e);}
 });
 
 module.exports = router;
